17e976c985fce444c578030deb243dccde403e98
Felix Kaiser <felix.kaiser@fxkr.net>
2017-03-05 20:03:48 +0100

    Import minimum viable product

---
+++ netbox-serve.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+import json
+import os
+import urllib.parse
+
+import jinja2
+
+import psycopg2
+import psycopg2.extras
+
+from flask import Flask, g, jsonify
+from flask_basicauth import BasicAuth
+
+
+app = Flask(__name__)
+basic_auth = BasicAuth(app)
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        url = urllib.parse.urlparse(os.environ["DATABASE_URL"])
+        db = g._database = psycopg2.connect(
+            database=url.path[1:],
+            user=url.username,
+            password=url.password,
+            host=url.hostname,
+            port=url.port)
+    return db
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+@app.route("/mappings")
+@basic_auth.required
+def get_zone():
+
+    records = {}
+
+    with get_db().cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+        cur.execute("""\
+            SELECT DISTINCT
+                ipam_ipaddress.address as i_address,
+                dcim_device.name as d_name
+            FROM
+                ipam_ipaddress
+            JOIN dcim_device ON ipam_ipaddress.id = dcim_device.primary_ip4_id
+            JOIN tenancy_tenant ON ipam_ipaddress.tenant_id = tenancy_tenant.id
+            WHERE
+                bool(ipam_ipaddress.status) AND
+                bool(dcim_device.status) AND
+                ipam_ipaddress.family = 4 AND
+                tenancy_tenant.slug = %s
+            ORDER BY
+                ipam_ipaddress.address ASC,
+                dcim_device.name ASC
+        """, (app.config["NETBOX_TENANT_SLUG"],))
+
+        result = {row["d_name"].lower(): row["i_address"].ip.compressed for row in cur}
+        return jsonify(result)
+
+
+def main():
+
+    psycopg2.extras.register_ipaddress()
+
+    app.config.from_mapping(**os.environ)
+    app.run(debug=True)
+
+
+if __name__ == "__main__":
+    main()
---
+++ netbox-update.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+
+import datetime
+import ipaddress
+import os
+import sys
+import tempfile
+
+import jinja2
+import requests
+import dns
+import dns.zone
+
+
+def main():
+    vars = {key.lower(): value for (key, value) in os.environ.items()}
+
+    try:
+        resp = requests.get(os.environ["NETBOX_ENDPOINT"])
+        resp.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        print(err, file=sys.stderr)
+        sys.exit(1)
+
+    forward_records = []
+    reverse_records = {}
+
+    data = resp.json()
+    for name, ip_str in data.items():
+        ip = ipaddress.ip_address(ip_str)
+        block = ".".join(reversed(ip.compressed.split(".")[:3]))
+        forward_records.append((name, "A", ip.compressed))
+        if block not in reverse_records:
+            reverse_records[block] = []
+        reverse_records[block].append((ip.reverse_pointer + ".", "PTR", name + os.environ["DNS_ZONE"] + "."))
+
+    origin_records = [("NS", name.strip()) for name in os.environ["DNS_SERVERS"].split(",")]
+
+    update_zonefile(os.path.join(os.environ["DNS_DIRECTORY"], os.environ["DNS_ZONE"]), origin_records, os.environ["DNS_ZONE"], forward_records)
+
+    for block, block_reverse_records in reverse_records.items():
+        update_zonefile(os.path.join(os.environ["DNS_DIRECTORY"], block), origin_records, block + ".in-addr.arpa", block_reverse_records)
+
+
+def update_zonefile(path, origin_records, zone_name, records):
+
+    zone = dns.zone.from_text(open(path))
+    origin_node = zone.get("@")
+    soa_rdataset = origin_node.get_rdataset(dns.rdataclass.IN, dns.rdatatype.SOA)
+    previous_serial = soa_rdataset.items[0].serial
+
+    new_serial = todays_serial = int(datetime.datetime.today().strftime("%Y%m%d00"))
+    if os.path.exists(path):
+        new_serial = max(previous_serial + 1, todays_serial)
+    new_serial_str = str(new_serial)
+    assert len(new_serial_str) == len("2000123100")
+
+    vars = {
+        "dns_zone": zone_name,
+        "dns_contact": os.environ["DNS_CONTACT"],
+
+        "dns_serial": new_serial_str,
+        "dns_refresh_time": os.environ["DNS_REFRESH_TIME"],
+        "dns_retry_time": os.environ["DNS_RETRY_TIME"],
+        "dns_expire_time": os.environ["DNS_EXPIRE_TIME"],
+        "dns_negative_cache_time": os.environ["DNS_NEGATIVE_CACHE_TIME"],
+
+        "dns_servers": [s.strip() for s in os.environ["DNS_SERVERS"].split(",")],
+        "dns_source": "netbox",
+
+        "origin_records": origin_records,
+        "records": records,
+    }
+
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader("."), undefined=jinja2.StrictUndefined)
+    text = env.get_template("forward.j2").render(vars)
+
+    with tempfile.NamedTemporaryFile(dir=os.path.dirname(path), delete=False) as temp_file:
+        temp_file.write(env.get_template("zonefile.j2").render(vars).encode("utf-8"))
+    os.replace(temp_file.name, path)
+
+
+if __name__ == "__main__":
+    main()
